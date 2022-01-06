54b351769e11a6b91fbe12764235a24c23652829
DeathWalker47 <fizer_00@mail.ru>
2022-01-06 12:31:00 +0300

    Добавил сдайлер

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,36 @@
-$(function(){
-  
+// $(function(){
+
+// });
+
+new Swiper(".slider-work", {
+  loop: true,
+  spaceBetween: 0,
+  speed: 1000,
+  slidesPerView: 4,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+    formatFractionCurrent: function (number) {
+      if (number < 100) {
+        number = "0" + number;
+      }
+      if (number < 10) {
+        number = "0" + number;
+      }
+      return number;
+    },
+    formatFractionTotal: function (number) {
+      if (number < 100) {
+        number = "0" + number;
+      }
+      if (number < 10) {
+        number = "0" + number;
+      }
+      return number;
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
