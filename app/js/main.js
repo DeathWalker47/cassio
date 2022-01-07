e248d1533b98ca4dd3c118eb28bf768b03b59dbe
DeathWalker47 <fizer_00@mail.ru>
2022-01-07 14:14:44 +0300

    Добавил несколько секция для главной страницы

--- app/js/main.js
+++ app/js/main.js
@@ -34,3 +34,36 @@ new Swiper(".slider-work", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+new Swiper(".slider-reviews", {
+  loop: true,
+  spaceBetween: 0,
+  speed: 800,
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+    formatFractionCurrent: function (number) {
+      if (number < 100) {
+        number = "0" + number;
+      }
+      if (number < 10) {
+        number = "0" + number;
+      }
+      return number;
+    },
+    formatFractionTotal: function (number) {
+      if (number < 100) {
+        number = "0" + number;
+      }
+      if (number < 10) {
+        number = "0" + number;
+      }
+      return number;
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
