8eaea9b5710f0fa1ff459bfddccf04a3ecdfcc44
DeathWalker47 <fizer_00@mail.ru>
2022-01-23 12:24:35 +0300

    Сделал выпадающее меню

--- app/js/main.js
+++ app/js/main.js
@@ -2,12 +2,32 @@
 
 // });
 
+document.body.style.maxWidth =  document.body.scrollWidth + 'px'
+
 const btnMenu = document.querySelector(".burger-btn");
 const headerMenu = document.querySelector(".heade-menu");
+
+const linkDropdown = document.querySelector(".menu__link--dropdown");
+const menuList = document.querySelector(".menu__list");
+const menuDropdown = document.querySelector(".menu__list--dropdown");
+const linkBack = document.querySelector(".item-back__link");
+
 btnMenu.addEventListener("click", () => {
   btnMenu.classList.toggle("active");
   headerMenu.classList.toggle("active");
-  document.querySelector('body').classList.toggle('hidden');
+  document.querySelector("body").classList.toggle("hidden");
+  menuList.classList.remove("transform");
+  menuDropdown.classList.remove("transform");
+});
+
+linkDropdown.addEventListener("click", () => {
+  menuList.classList.add("transform");
+  menuDropdown.classList.add("transform");
+});
+
+linkBack.addEventListener("click", () => {
+  menuList.classList.remove("transform");
+  menuDropdown.classList.remove("transform");
 });
 
 new Swiper(".slider-work", {
@@ -75,39 +95,6 @@ new Swiper(".platinum-slider", {
     prevEl: ".swiper-button-prev",
   },
 });
-
-// new Swiper(".royal-slider", {
-//   loop: true,
-//   spaceBetween: 30,
-//   speed: 1000,
-//   slidesPerView: 1,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     type: "fraction",
-//     formatFractionCurrent: function (number) {
-//       if (number < 100) {
-//         number = "0" + number;
-//       }
-//       if (number < 10) {
-//         number = "0" + number;
-//       }
-//       return number;
-//     },
-//     formatFractionTotal: function (number) {
-//       if (number < 100) {
-//         number = "0" + number;
-//       }
-//       if (number < 10) {
-//         number = "0" + number;
-//       }
-//       return number;
-//     },
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
 
 new Swiper(".slider-reviews", {
   loop: true,
